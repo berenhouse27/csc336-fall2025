989f9a3048716122af278e30c8859246ae432636
berenhouse27 <berenhouse23@gmail.com>
2025-10-14 15:42:44 -0400

    commit

--- 07-json-world/world.js
+++ 07-json-world/world.js
@@ -3,12 +3,50 @@ import * as fs from 'fs';
 const data = fs.readFileSync('./world.json', 'utf-8');
 const world = JSON.parse(data);
 
-console.log("Notable people:");
-
+console.log("Men of Arlen:");
 world.regions.forEach(region => {
   region.towns.forEach(town => {
+    console.log(town.name + ':')
     town["notable people"].forEach(person => {
-      console.log(person.name);
-    });
-  });
-});
+        if (person.gender == 'male') {
+            console.log(`   ${person.name} (${person.role})`)
+            person.items.forEach( item => {
+                if (typeof item === "object") {
+                    console.log('         -' + item.name)
+                    const attributes = Object.entries(item)
+                    for (const entry of attributes){
+                        if (entry[0] != 'name')
+                            console.log('             ' + entry[0] + ': ' + entry[1])
+                    }
+                }
+                else
+                    console.log('         -' + item)
+            })
+        }
+        })
+    })
+})
+console.log('')
+console.log("Women of Arlen:");
+world.regions.forEach(region => {
+  region.towns.forEach(town => {
+    console.log(town.name + ':')
+    town["notable people"].forEach(person => {
+        if (person.gender == 'female') {
+            console.log(`   ${person.name} (${person.role})`)
+            person.items.forEach( item => {
+                if (typeof item === "object") {
+                    console.log('         -' + item.name)
+                    const attributes = Object.entries(item)
+                    for (const entry of attributes){
+                        if (entry[0] != 'name')
+                            console.log('             ' + entry[0] + ': ' + entry[1])
+                    }
+                }
+                else
+                    console.log('         -' + item)
+            })
+        }
+        })
+    })
+})
