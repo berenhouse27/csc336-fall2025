f2cf43cdb4d8b3559927a46d34e32be64fc56aa4
berenhouse27 <berenhouse23@gmail.com>
2025-10-14 18:27:05 -0400

    formatting

--- 07-json-world/world.js
+++ 07-json-world/world.js
@@ -6,24 +6,24 @@ const world = JSON.parse(data);
 // Men of Arlen
 console.log("Men of Arlen:");
 world.regions.forEach(region => {
-  region.towns.forEach(town => {
-    console.log(town.name + ':')
-    town.notable_people.forEach(person => {
-        if (person.gender == 'male') {
-            console.log(`   ${person.name} (${person.role})`)
-            person.items.forEach( item => {
-                if (typeof item === "object") {
-                    console.log('         -' + item.name)
-                    const attributes = Object.entries(item)
-                    for (const entry of attributes){
-                        if (entry[0] != 'name')
-                            console.log('             ' + entry[0] + ': ' + entry[1])
+    region.towns.forEach(town => {
+        console.log(town.name + ':')
+        town.notable_people.forEach(person => {
+            if (person.gender == 'male') {
+                console.log(`   ${person.name} (${person.role})`)
+                person.items.forEach( item => {
+                    if (typeof item === "object") {
+                        console.log('         -' + item.name)
+                        const attributes = Object.entries(item)
+                        for (const entry of attributes){
+                            if (entry[0] != 'name')
+                                console.log('             ' + entry[0] + ': ' + entry[1])
+                        }
                     }
-                }
-                else
-                    console.log('         -' + item)
-            })
-        }
+                    else
+                        console.log('         -' + item)
+                })
+            }
         })
     })
 })
@@ -33,24 +33,24 @@ console.log('')
 // Women of Arlen
 console.log("Women of Arlen:");
 world.regions.forEach(region => {
-  region.towns.forEach(town => {
-    console.log(town.name + ':')
-    town.notable_people.forEach(person => {
-        if (person.gender == 'female') {
-            console.log(`   ${person.name} (${person.role})`)
-            person.items.forEach( item => {
-                if (typeof item === "object") {
-                    console.log('         -' + item.name)
-                    const attributes = Object.entries(item)
-                    for (const entry of attributes){
-                        if (entry[0] != 'name')
-                            console.log('             ' + entry[0] + ': ' + entry[1])
+    region.towns.forEach(town => {
+        console.log(town.name + ':')
+        town.notable_people.forEach(person => {
+            if (person.gender == 'female') {
+                console.log(`   ${person.name} (${person.role})`)
+                person.items.forEach( item => {
+                    if (typeof item === "object") {
+                        console.log('         -' + item.name)
+                        const attributes = Object.entries(item)
+                        for (const entry of attributes){
+                            if (entry[0] != 'name')
+                                console.log('             ' + entry[0] + ': ' + entry[1])
+                        }
                     }
-                }
-                else
-                    console.log('         -' + item)
+                    else
+                        console.log('         -' + item)
+                })
+            }
             })
-        }
-        })
     })
 })
