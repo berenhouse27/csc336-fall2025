594ae96a6532adaf4ee452ef233471f8d09b5f7c
berenhouse27 <berenhouse23@gmail.com>
2025-10-28 13:07:08 -0400

    Update server.js

--- 08-server-world/server.js
+++ 08-server-world/server.js
@@ -35,4 +35,5 @@ app.post("/update", async (req, res) => {
     res.json(world);
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
